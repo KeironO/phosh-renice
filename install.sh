0a0a9a62cde58e0b06fe92f7505ba910e476ae08
Djhg2000 <github@djhg2000.net>
2020-08-10 13:46:57 +0200

    Add installer script

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# The UID variable is only available in bash
+if [ $UID -ne 0 ]; then
+	echo "This script needs to be run as root"
+	exit 1
+fi
+
+if [ ! -d /usr/local/sbin ]; then
+	echo "Directory /usr/local/sbin doesn't exist, creating..."
+	if ! mkdir -p /usr/local/sbin; then
+		echo "Failed to create /usr/local/sbin, aborting"
+		exit 1
+	fi
+fi
+
+echo "Copying files..."
+if ! cp phosh_renice.sh /usr/local/sbin/phosh_renice; then
+	echo "Failed to copy phosh_renice.sh, aborting"
+	exit 1
+fi
+
+if ! cp phosh_renice.service /etc/systemd/system/; then
+	echo "Failed to copy phosh_renice.service, aborting"
+	exit 1
+fi
+
+echo "Starting service"
+if ! systemctl start phosh_renice; then
+	echo "Failed to start the phosh_renice service, aborting"
+	exit 1
+fi
+
+echo "Enabling autostart"
+if ! systemctl enable phosh_renice; then
+	echo "Failed to enable the phosh_renice service, aborting"
+	exit 1
+fi
+
+echo "Successfully installed the phosh_renice service."
+
